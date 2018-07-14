44c396180e9f53289813849a0b83f994bca030c1
DESKTOP-KOOTIL8\boatc <boatclubonline@gmail.com>
2018-07-14 08:39:58 +0700

    commit

--- src/main/java/com/example/SpringBootApplication.java
+++ src/main/java/com/example/SpringBootApplication.java
@@ -1,12 +1,12 @@
-package com.example.demo;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SpringBootApplication.class, args);
 	}
 }
---
+++ src/main/java/com/example/dao/StudentRepo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.dao;
+
+import com.example.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author boatc
+ */
+@Repository
+public class StudentRepo {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public Student findById(long id) {
+        return jdbcTemplate.queryForObject("select * from student where id=?", new Object[]{id}, new BeanPropertyRowMapper< Student>(Student.class));
+    }
+
+}
