bc553492dc2c65baa35dbe5255d6bb12385f064b
DESKTOP-KOOTIL8\boatc <boatclubonline@gmail.com>
2018-07-14 08:41:24 +0700

    commit

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootApplication {
+public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplication.class, args);
+    }
 }
